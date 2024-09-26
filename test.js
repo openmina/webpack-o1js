32651337b647fb76b8a1eeb660f717cc3894cb07
Teofil Jolte <teofil@Teofils-MacBook-Pro.local>
2024-09-26 13:07:40 +0300

    Zk Apps

---
+++ test.js
@@ -0,0 +1,7 @@
+// const bootstrap = require('../dist/bootstrap.js');
+
+// iife for logging directly on load.
+(async () => {
+	console.log('Deployment successful');
+	// console.log('bootstrap', bootstrap);
+})();
